8e63978dd39ba8bbfbcf15b9a7d7f701a07e721e
ashabelnikov <ashabelnikov@c1dae7de-a428-49f4-85d8-50b65f41d01d>
2008-09-27 09:48:25 +0000

    Firmware data import/export dialog (view).
    git-svn-id: http://svn2.assembla.com/svn/secu3man@29 c1dae7de-a428-49f4-85d8-50b65f41d01d

--- sources/FWImpExp/MapImpExpDlg.cpp
+++ sources/FWImpExp/MapImpExpDlg.cpp
@@ -26,10 +26,11 @@ CMapImpExpDlg::CMapImpExpDlg(CWnd* pParent /*=NULL*/)
 	//{{AFX_DATA_INIT(CMapImpExpDlg)
 	m_current_fwd_title_string = _T("");
 	m_other_fwd_title_string = _T("");
-	m_idle_map_flag = FALSE;
-	m_start_map_flag = FALSE;
-	m_temp_map_flag = FALSE;
-	m_work_map_flag = FALSE;
+		
+	m_fwd_flags[FLAG_START_MAP] = FALSE;
+	m_fwd_flags[FLAG_IDLE_MAP] = FALSE;
+	m_fwd_flags[FLAG_WORK_MAP] = FALSE;
+	m_fwd_flags[FLAG_TEMP_MAP] = FALSE;
 	//}}AFX_DATA_INIT
 }
 
@@ -38,14 +39,19 @@ void CMapImpExpDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
 	//{{AFX_DATA_MAP(CMapImpExpDlg)
-	DDX_Control(pDX, IDC_MAP_IMPEXP_OTHER_FWD_LIST, m_other_fwd_list);
 	DDX_Control(pDX, IDC_MAP_IMPEXP_CURRENT_FWD_LIST, m_current_fwd_list);
+	DDX_Control(pDX, IDC_MAP_IMPEXP_OTHER_FWD_LIST, m_other_fwd_list);
 	DDX_Text(pDX, IDC_MAP_IMPEXP_CURRENT_FWD_TITLE, m_current_fwd_title_string);
 	DDX_Text(pDX, IDC_MAP_IMPEXP_OTHER_FWD_TITLE, m_other_fwd_title_string);
-	DDX_Check(pDX, IDC_MAP_IMPEXP_IDLEMAP_FLAG, m_idle_map_flag);
-	DDX_Check(pDX, IDC_MAP_IMPEXP_STARTMAP_FLAG, m_start_map_flag);
-	DDX_Check(pDX, IDC_MAP_IMPEXP_TEMPMAP_FLAG, m_temp_map_flag);
-	DDX_Check(pDX, IDC_MAP_IMPEXP_WORKMAP_FLAG, m_work_map_flag);
+	DDX_Check(pDX, IDC_MAP_IMPEXP_STARTMAP_FLAG,m_fwd_flags[FLAG_START_MAP]);
+	DDX_Check(pDX, IDC_MAP_IMPEXP_IDLEMAP_FLAG, m_fwd_flags[FLAG_IDLE_MAP]);
+	DDX_Check(pDX, IDC_MAP_IMPEXP_WORKMAP_FLAG, m_fwd_flags[FLAG_WORK_MAP]);
+	DDX_Check(pDX, IDC_MAP_IMPEXP_TEMPMAP_FLAG, m_fwd_flags[FLAG_TEMP_MAP]);
+
+	DDX_Control(pDX, IDC_MAP_IMPEXP_STARTMAP_FLAG,m_fwd_flags_buttons[FLAG_START_MAP]);
+	DDX_Control(pDX, IDC_MAP_IMPEXP_IDLEMAP_FLAG, m_fwd_flags_buttons[FLAG_IDLE_MAP]);
+	DDX_Control(pDX, IDC_MAP_IMPEXP_WORKMAP_FLAG, m_fwd_flags_buttons[FLAG_WORK_MAP]);
+	DDX_Control(pDX, IDC_MAP_IMPEXP_TEMPMAP_FLAG, m_fwd_flags_buttons[FLAG_TEMP_MAP]);
 	//}}AFX_DATA_MAP
 }
 
@@ -61,6 +67,138 @@ END_MESSAGE_MAP()
 
 void CMapImpExpDlg::OnMapImpexpExchangeButton() 
 {
-	// TODO: Add your control notification handler code here
-	
+ if (m_OnExchangeButton)
+   m_OnExchangeButton();	 	
+}
+
+//----------------------------------------------------------------------------
+//изменилось выделение в спимке семейств характеристик
+void CMapImpExpDlg::OnChangeFWDCurrentList(NMHDR* pNMHDR, LRESULT* pResult) 
+{
+  NMLISTVIEW *pnmv = (NM_LISTVIEW FAR *) pNMHDR;
+  
+  if (pnmv->uChanged == LVIF_STATE && (pnmv->uNewState & LVIS_SELECTED)) 
+  {
+   //состояние выделения изменилось
+   int selected_index = 0;
+
+   //will return -1 if there are no selected items, at all
+   selected_index = m_current_fwd_list.GetNextItem(-1, LVNI_SELECTED);
+
+   if (m_OnFWDCurrentSelectionChanged)
+	 m_OnFWDCurrentSelectionChanged(selected_index);     
+  }
+ 
+  *pResult = 0;
+}
+
+void CMapImpExpDlg::OnEndLabelEditFWDCurrentList(NMHDR* pNMHDR, LRESULT* pResult) 
+{
+  NMLVDISPINFO *pdi = (LV_DISPINFO FAR *) pNMHDR;
+
+  if (pdi->item.mask == LVIF_TEXT) 
+  {
+   m_current_fwd_list.SetItem(&pdi->item);
+   if (m_OnFWDCurrentListNamechanged)
+     m_OnFWDCurrentListNamechanged(pdi->item.iItem,pdi->item.pszText); 
+  }
+}
+
+void CMapImpExpDlg::FillFWDCurrentList(std::vector<_TSTRING> i_list_of_names)
+{
+  m_current_fwd_list.DeleteAllItems();
+  for (size_t i = 0; i < i_list_of_names.size(); i++)
+  {
+   m_current_fwd_list.InsertItem(i,i_list_of_names[i].c_str());	
+  }
+}
+
+void CMapImpExpDlg::SetFWDCurrentListSelection(int i_selected_index)
+{
+  if (i_selected_index!=-1)
+    m_current_fwd_list.SetItemState(i_selected_index, LVIS_SELECTED, LVIS_SELECTED);
+
+  int i = m_current_fwd_list.GetNextItem(-1, LVNI_SELECTED);
+}
+
+void CMapImpExpDlg::SetFWDCurrentListTitle(const _TSTRING& i_title)
+{
+  m_current_fwd_title_string = i_title.c_str(); 
+  UpdateData(FALSE);
+}
+
+
+
+//----------------------------------------------------------------------------
+//изменилось выделение в спимке семейств характеристик
+void CMapImpExpDlg::OnChangeFWDOtherList(NMHDR* pNMHDR, LRESULT* pResult) 
+{
+  NMLISTVIEW *pnmv = (NM_LISTVIEW FAR *) pNMHDR;
+  
+  if (pnmv->uChanged == LVIF_STATE && (pnmv->uNewState & LVIS_SELECTED)) 
+  {
+   //состояние выделения изменилось
+   int selected_index = 0;
+
+   //will return -1 if there are no selected items, at all
+   selected_index = m_current_fwd_list.GetNextItem(-1, LVNI_SELECTED);
+
+   if (m_OnFWDCurrentSelectionChanged)
+	 m_OnFWDCurrentSelectionChanged(selected_index);     
+  }
+ 
+  *pResult = 0;
+}
+
+void CMapImpExpDlg::OnEndLabelEditFWDOtherList(NMHDR* pNMHDR, LRESULT* pResult) 
+{
+  NMLVDISPINFO *pdi = (LV_DISPINFO FAR *) pNMHDR;
+
+  if (pdi->item.mask == LVIF_TEXT) 
+  {
+   m_current_fwd_list.SetItem(&pdi->item);
+   if (m_OnFWDCurrentListNamechanged)
+     m_OnFWDCurrentListNamechanged(pdi->item.iItem,pdi->item.pszText); 
+  }
+}
+
+void CMapImpExpDlg::FillFWDOtherList(std::vector<_TSTRING> i_list_of_names)
+{
+  m_current_fwd_list.DeleteAllItems();
+  for (size_t i = 0; i < i_list_of_names.size(); i++)
+  {
+   m_current_fwd_list.InsertItem(i,i_list_of_names[i].c_str());	
+  }
+}
+
+void CMapImpExpDlg::SetFWDOtherListSelection(int i_selected_index)
+{
+  if (i_selected_index!=-1)
+    m_current_fwd_list.SetItemState(i_selected_index, LVIS_SELECTED, LVIS_SELECTED);
+
+  int i = m_current_fwd_list.GetNextItem(-1, LVNI_SELECTED);
+}
+
+void CMapImpExpDlg::SetFWDOtherListTitle(const _TSTRING& i_title)
+{
+  m_other_fwd_title_string = i_title.c_str();
+  UpdateData(FALSE);
+}
+
+
+void CMapImpExpDlg::SetFWDFlag(EFWDFlags i_flag_type, bool i_state)
+{
+ m_fwd_flags[i_flag_type] = i_state ? TRUE : FALSE;
+ UpdateData(FALSE);
+}
+
+bool CMapImpExpDlg::GetFWDFlag(EFWDFlags i_flag_type)
+{
+  return m_fwd_flags[i_flag_type] ? true : false;
+}
+
+void CMapImpExpDlg::EnableFWDFlag(EFWDFlags i_flag_type, bool i_enable)
+{
+  BOOL enable = i_enable ? TRUE : FALSE;
+  m_fwd_flags_buttons[i_flag_type].EnableWindow(enable);
 }
--- sources/FWImpExp/MapImpExpDlg.h
+++ sources/FWImpExp/MapImpExpDlg.h
@@ -2,41 +2,80 @@
 #pragma once
 
 #include "resource.h"
+#include "common/FastDelegate.h"
+#include <vector>
+#include "common/UnicodeSupport.h"
+
+
+enum EFWDFlags
+{
+  FLAG_START_MAP = 0,
+  FLAG_IDLE_MAP,
+  FLAG_WORK_MAP,
+  FLAG_TEMP_MAP,
+  FLAG_NR_OF_FLAGS
+};
+
 
 class CMapImpExpDlg : public CDialog
 {
-// Construction
+
 public:
+    typedef fastdelegate::FastDelegate0<> EventHandler;
+	typedef fastdelegate::FastDelegate2<int,CString> EventWithCodeAndString;
+	typedef fastdelegate::FastDelegate1<int> EventWithCode;
+
 	CMapImpExpDlg(CWnd* pParent = NULL);   // standard constructor
 
-// Dialog Data
+	void setOnExchangeButton(EventHandler OnFunction) {m_OnExchangeButton = OnFunction;};
+	void setOnFWDCurrentListNamechanged(EventWithCodeAndString OnFunction) {m_OnFWDCurrentListNamechanged = OnFunction;};
+	void setOnFWDCurrentSelectionChanged(EventWithCode OnFunction) {m_OnFWDCurrentSelectionChanged = OnFunction;};
+
+	void FillFWDCurrentList(std::vector<_TSTRING> i_list_of_names);
+	void SetFWDCurrentListSelection(int i_selected_index);
+	void SetFWDCurrentListTitle(const _TSTRING& i_title);
+
+	void FillFWDOtherList(std::vector<_TSTRING> i_list_of_names);
+	void SetFWDOtherListSelection(int i_selected_index);
+	void SetFWDOtherListTitle(const _TSTRING& i_title);
+
+	void SetFWDFlag(EFWDFlags i_flag_type, bool i_state);
+	bool GetFWDFlag(EFWDFlags i_flag_type);
+	void EnableFWDFlag(EFWDFlags i_flag_type, bool i_enable);
+
+    // Dialog Data
 	//{{AFX_DATA(CMapImpExpDlg)
 	enum { IDD = IDD_MAP_IMPEXP };
 	CListCtrl	m_other_fwd_list;
 	CListCtrl	m_current_fwd_list;
 	CString	m_current_fwd_title_string;
 	CString	m_other_fwd_title_string;
-	BOOL	m_idle_map_flag;
-	BOOL	m_start_map_flag;
-	BOOL	m_temp_map_flag;
-	BOOL	m_work_map_flag;
+	BOOL    m_fwd_flags[FLAG_NR_OF_FLAGS];
+	CButton m_fwd_flags_buttons[FLAG_NR_OF_FLAGS];
 	//}}AFX_DATA
 
 
-// Overrides
-	// ClassWizard generated virtual function overrides
+    // Overrides
 	//{{AFX_VIRTUAL(CMapImpExpDlg)
 	protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 	//}}AFX_VIRTUAL
 
-// Implementation
+    // Implementation
 protected:
 
-	// Generated message map functions
 	//{{AFX_MSG(CMapImpExpDlg)
 	afx_msg void OnMapImpexpExchangeButton();
+	afx_msg void OnChangeFWDCurrentList(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnEndLabelEditFWDCurrentList(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnChangeFWDOtherList(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnEndLabelEditFWDOtherList(NMHDR* pNMHDR, LRESULT* pResult);
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
+
+private:
+	EventHandler m_OnExchangeButton;
+	EventWithCodeAndString m_OnFWDCurrentListNamechanged;
+	EventWithCode m_OnFWDCurrentSelectionChanged;
 };
 
