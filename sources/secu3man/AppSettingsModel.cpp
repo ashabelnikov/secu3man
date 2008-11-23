dc5b451cde64e14e61d8771315047382674cf075
ashabelnikov <ashabelnikov@c1dae7de-a428-49f4-85d8-50b65f41d01d>
2008-11-23 14:53:44 +0000

    256000 baud value has been added
    git-svn-id: http://svn2.assembla.com/svn/secu3man@54 c1dae7de-a428-49f4-85d8-50b65f41d01d

--- sources/secu3man/AppSettingsModel.cpp
+++ sources/secu3man/AppSettingsModel.cpp
@@ -43,6 +43,7 @@ CAppSettingsModel::CAppSettingsModel()
   m_AllowableBaudRates.push_back(CBR_56000);
   m_AllowableBaudRates.push_back(CBR_57600);
   m_AllowableBaudRates.push_back(CBR_115200);
+  m_AllowableBaudRates.push_back(CBR_256000);
 
   //заполняем базу данных допустимых COM-портов
   for(int i = 1; i < 256; i++)
