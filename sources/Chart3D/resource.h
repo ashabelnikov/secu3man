0ccaa4c12fa007fb359c520afa897cfe5060ebf8
ashabelnikov <shabelnikov@secu-3.org>
2013-02-23 22:11:47 +0200

    Code clean up

--- sources/Chart3D/resource.h
+++ sources/Chart3D/resource.h
@@ -1,11 +1,13 @@
+// Resource header file for Chart3D DLL unit.
+// Created by Alexey A. Shabelnikov, UA, Kiev. http://secu-3.org
 
-#define IDS_EDITING_MAPS  100
-#define IDS_LEFT_AXIS_TITLE 101
-#define IDS_AIR_FLOW_CAPTION_TEXT 102
-#define IDS_BACK_SIDE_VIEW_CB 103
-#define IDS_PM_ZERO_ALL_POINTS 104
-#define IDS_PM_SET_ALLTO_1ST_PT 105
-#define IDS_PM_BLD_CURVE_1ST_LAST_PT 106
-#define IDS_PM_ZERO_ALL_CURVES 107
-#define IDS_PM_SET_ALLTO_THIS_CR 108
-#define IDS_PM_BLD_SHAPE_1ST_LAST_CR 109
+#define IDS_EDITING_MAPS               100
+#define IDS_LEFT_AXIS_TITLE            101
+#define IDS_AIR_FLOW_CAPTION_TEXT      102
+#define IDS_BACK_SIDE_VIEW_CB          103
+#define IDS_PM_ZERO_ALL_POINTS         104
+#define IDS_PM_SET_ALLTO_1ST_PT        105
+#define IDS_PM_BLD_CURVE_1ST_LAST_PT   106
+#define IDS_PM_ZERO_ALL_CURVES         107
+#define IDS_PM_SET_ALLTO_THIS_CR       108
+#define IDS_PM_BLD_SHAPE_1ST_LAST_CR   109
