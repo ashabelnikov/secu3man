2795a5dbdf405b04886e7421a1853da93bef41cd
uzZ <mishkuroff@gmail.com>
2013-03-21 22:50:06 +0200

    Added tooltips for CE panel on first tab (not tested yet)

--- sources/MIdesk/CEDeskDlg.cpp
+++ sources/MIdesk/CEDeskDlg.cpp
@@ -63,7 +63,18 @@ BOOL CCEDeskDlg::OnInitDialog()
  //create a tooltip control and assign tooltips
  mp_ttc.reset(new CToolTipCtrlEx());
  VERIFY(mp_ttc->Create(this, WS_POPUP | TTS_ALWAYSTIP | TTS_BALLOON));
- VERIFY(mp_ttc->AddWindow(this, MLL::GetString(IDS_CE_ERRORS_DESK_TT)));
+ //VERIFY(mp_ttc->AddWindow(this, MLL::GetString(IDS_CE_ERRORS_DESK_TT)));
+ VERIFY(mp_ttc->AddWindow(&m_ce_errors[0], MLL::GetString(IDS_CE_ERRORS_DESK_CKPS_MALFUNCTION_TT)));
+ VERIFY(mp_ttc->AddWindow(&m_ce_errors[1], MLL::GetString(IDS_CE_ERRORS_DESK_EEPROM_PARAM_BROKEN_TT)));
+ VERIFY(mp_ttc->AddWindow(&m_ce_errors[2], MLL::GetString(IDS_CE_ERRORS_DESK_PROGRAM_CODE_BROKEN_TT)));
+ VERIFY(mp_ttc->AddWindow(&m_ce_errors[3], MLL::GetString(IDS_CE_ERRORS_DESK_KSP_CHIP_FAILED_TT)));
+ VERIFY(mp_ttc->AddWindow(&m_ce_errors[4], MLL::GetString(IDS_CE_ERRORS_DESK_KNOCK_DETECTED_TT)));
+ VERIFY(mp_ttc->AddWindow(&m_ce_errors[5], MLL::GetString(IDS_CE_ERRORS_DESK_MAP_SENSOR_FAIL_TT)));
+ VERIFY(mp_ttc->AddWindow(&m_ce_errors[6], MLL::GetString(IDS_CE_ERRORS_DESK_TEMP_SENSOR_FAIL_TT)));
+ VERIFY(mp_ttc->AddWindow(&m_ce_errors[7], MLL::GetString(IDS_CE_ERRORS_DESK_VOLT_SENSOR_FAIL_TT)));
+ VERIFY(mp_ttc->AddWindow(&m_ce_errors[8], MLL::GetString(IDS_CE_ERRORS_DWELL_CONTROL_TT)));
+ VERIFY(mp_ttc->AddWindow(&m_ce_errors[9], MLL::GetString(IDS_CE_ERRORS_DESK_CAMS_MALFUNCTION_TT)));
+ VERIFY(mp_ttc->AddWindow(&m_ce_errors[10], MLL::GetString(IDS_CE_ERRORS_DESK_TPS_SENSOR_FAIL_TT)));
  mp_ttc->SetMaxTipWidth(100); //Enable text wrapping
  mp_ttc->ActivateToolTips(true);
 
