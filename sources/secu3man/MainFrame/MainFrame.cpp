7d194f086331797c1104f939edd9836b5cc948b9
ashabelnikov <shabelnikov@secu-3.org>
2013-03-21 21:27:14 +0200

    Increase size of main window little more

--- sources/secu3man/MainFrame/MainFrame.cpp
+++ sources/secu3man/MainFrame/MainFrame.cpp
@@ -160,11 +160,11 @@ BOOL CMainFrame::PreCreateWindow(CREATESTRUCT& cs)
  switch(dpia.GetDPIY())
  {
   case 72:
-   cs.cy = dpia.ScaleY(605);
+   cs.cy = dpia.ScaleY(607);
    break;
   default:
   case 96:
-   cs.cy = dpia.ScaleY(475);
+   cs.cy = dpia.ScaleY(477);
    break;
  }
 
