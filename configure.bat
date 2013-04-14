ef8f9f2f558715b929faa20cb46291a752ac34af
ashabelnikov <shabelnikov@secu-3.org>
2013-04-14 15:16:33 +0300

    Add nmake run template to batch file

--- configure.bat
+++ configure.bat
@@ -8,6 +8,7 @@ cd secu3man
 
 cmake ../../sources/secu3man
 rem cmake -G "Visual Studio 7 .NET 2003" ../../sources/secu3man
+rem cmake -G "NMake Makefiles" ../../sources/secu3man
 
 cd ..\..\
 md libraries
