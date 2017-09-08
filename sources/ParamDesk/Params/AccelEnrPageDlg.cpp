0f737313957eaf0e48be95dd581c818636ab775a
ashabelnikov <shabelnikov@secu-3.org>
2017-09-08 14:03:15 +0300

    Add decay time for AE

--- sources/ParamDesk/Params/AccelEnrPageDlg.cpp
+++ sources/ParamDesk/Params/AccelEnrPageDlg.cpp
@@ -117,8 +117,8 @@ BOOL CAccelEnrPageDlg::OnInitDialog()
  m_decaytime_spin.SetBuddy(&m_decaytime_edit);
  m_decaytime_edit.SetLimitText(3);
  m_decaytime_edit.SetDecimalPlaces(3);
- m_decaytime_spin.SetRangeAndDelta(0, 255, 1);
- m_decaytime_edit.SetRange(0, 255);
+ m_decaytime_spin.SetRangeAndDelta(1, 255, 1);
+ m_decaytime_edit.SetRange(1, 255);
 
  //create a tooltip control and assign tooltips
  mp_ttc.reset(new CToolTipCtrlEx());
