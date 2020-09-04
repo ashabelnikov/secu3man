2290bb1168acd8e2f72d2020049ff535ddc3af95
Alexey A. Shabelnikov <shabelnikov@secu-3.org>
2020-09-04 20:01:59 +0300

    Add perameter for disabling idling ignition timing map

--- sources/io-core/SECU3IO.h
+++ sources/io-core/SECU3IO.h
@@ -768,6 +768,7 @@ const int INPAVNUM = 10;
   int vent_pwmsteps;
   int vent_minband;
   int an_tps_mul;
+  int igntim_wrkmap;
  };
 
  const float start_map_rpm_slots[16] = {200,240,280,320,360,400,440,480,520,560,600,640,680,720,760,800};
